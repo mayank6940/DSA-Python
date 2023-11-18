6522ff48d43028ebb26ab371e2288512223dd0ba
Mayank Raj <mayankraj287@gmail.com>
2023-11-19 01:33:38 +0530

    First Pro

--- recursion_1.py
+++ recursion_1.py
@@ -3,5 +3,11 @@ def factx(n):
         return 1
     return n * factx(n-1)
 
+def factx1(n):
+    if n == 0:
+        return 1
+    out = factx(n-1)
+    return n * out
+
 ent = int(input("Enter: "))
-print(factx(ent))
+print(factx1(ent))
